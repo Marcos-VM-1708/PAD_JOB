602a2d298a0dec6fbad34a39680c7d1a83067c95
Marcos <marcos.vinicios2@discente.ufg.br>
2023-07-23 14:54:54 -0300

    progess in scraping for selenium

--- Get/web_scraping.py
+++ Get/web_scraping.py
@@ -1,25 +1,21 @@
 import requests
-from bs4 import *
-from selenium import webdriver
+from bs4 import BeautifulSoup
 
-url = "https://www.google.com/maps/search/restaurants/"
-response = requests.get(url)
-#-----------------------
-
-def run_surch():
-    chrome_driver_path = "chromedriver.exe"
-
-    # Inicialize o navegador Chrome
-    driver = webdriver.Chrome(executable_path = chrome_driver_path)
-    driver.get(url)
+# Get the Google Maps search results page for "restaurantes em São Paulo"
+response = requests.get("https://www.google.com/maps/search/restaurantes+em+São+Paulo")
 
 
-def get_comment(url):
-    soup = BeautifulSoup(response.content, 'html.parser')
-    resultados = "html" # estrutura dos comentarios
-    for comentario in resultados:
-        # extrair os comentarios
-        pass
+# Parse the HTML response with BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+print(soup)
 
-if __name__ == '__main__':
-    run_surch()
+# Find all the restaurant results
+restaurants = soup.find_all("div", class_="from_the_business")
+print(restaurants)
+
+# Print the name, address, and rating of each restaurant
+for restaurant in restaurants:
+    name = restaurant.find("div", class_="business-name").text
+    address = restaurant.find("div", class_="business-address").text
+    rating = restaurant.find("div", class_="rating").text
+    print(f"Name: {name}\nAddress: {address}\nRating: {rating}")
