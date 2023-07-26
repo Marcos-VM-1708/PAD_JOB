1ad3613783311e09139cd5855b405b6e9948256d
Marcos <marcos.vinicios2@discente.ufg.br>
2023-07-26 11:34:47 -0300

    step two

--- Get/web_scraping.py
+++ Get/web_scraping.py
@@ -1,21 +1,41 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 
-# Get the Google Maps search results page for "restaurantes em São Paulo"
-response = requests.get("https://www.google.com/maps/search/restaurantes+em+São+Paulo")
+def scrape_google_maps_reviews(url):
+    # Configurar o Chrome WebDriver usando a variável de ambiente PATH
+    driver = webdriver.Chrome()
 
+    # Navegar até a página do local no Google Maps
+    driver.get(url)
 
-# Parse the HTML response with BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
-print(soup)
+    try:
+        # Esperar até que as avaliações sejam carregadas (pode variar dependendo da conexão)
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "section-review-content"))
+        )
 
-# Find all the restaurant results
-restaurants = soup.find_all("div", class_="from_the_business")
-print(restaurants)
+        # Obter todas as avaliações
+        reviews = driver.find_elements(By.CLASS_NAME, "section-review-content")
 
-# Print the name, address, and rating of each restaurant
-for restaurant in restaurants:
-    name = restaurant.find("div", class_="business-name").text
-    address = restaurant.find("div", class_="business-address").text
-    rating = restaurant.find("div", class_="rating").text
-    print(f"Name: {name}\nAddress: {address}\nRating: {rating}")
+        # Extrair o texto das avaliações
+        for review in reviews:
+            author_name = review.find_element(By.CLASS_NAME, "section-review-title").text
+            rating = review.find_element(By.CLASS_NAME, "section-review-stars").get_attribute("aria-label")
+            review_text = review.find_element(By.CLASS_NAME, "section-review-text").text
+
+            print(f"Author: {author_name}\nRating: {rating}\nReview: {review_text}\n")
+
+    except Exception as e:
+        print(f"Erro durante o scraping: {e}")
+
+    finally:
+        # Fechar o navegador após a conclusão do scraping
+        driver.quit()
+
+# Exemplo de uso:
+if __name__ == "__main__":
+    local_url = "https://www.google.com.br/maps/search/restaurants"
+    scrape_google_maps_reviews(local_url)
